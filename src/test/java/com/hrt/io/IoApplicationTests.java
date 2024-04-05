6a9405acb4bd3491d7365e2ae66beca46c2a5245
huangruntian <runtian.huang@tcl.com>
2024-04-05 16:33:34 +0800

    JavaIO流知识学习-加解密处理

--- src/test/java/com/hrt/io/IoApplicationTests.java
+++ src/test/java/com/hrt/io/IoApplicationTests.java
@@ -14,13 +14,27 @@ import java.io.IOException;
 class IoApplicationTests {
 
     @Test
-    void contextLoads() {
-        // 1. 创建对象表示数据源
-        File origin = new File("E:\\data");
-        // 2. 创建对象表示目的地
-        File target = new File("E:\\dataMirror");
-        // 3. 调用方法开始拷贝
-        mirrorFile(origin, target);
+    void contextLoads() throws IOException {
+        /**
+         * 拷贝文件
+         */
+//        // 1. 创建对象表示数据源
+//        File origin = new File("E:\\data");
+//        // 2. 创建对象表示目的地
+//        File target = new File("E:\\dataMirror");
+//        // 3. 调用方法开始拷贝
+//        mirrorFile(origin, target);
+
+        /**
+         * 加解密文件
+         */
+        File origin = new File("E:\\BaiduNetdiskDownload\\龙珠高清壁纸\\001.jpg");
+        File ency = new File("E:\\BaiduNetdiskDownload\\龙珠高清壁纸\\001ency.jpg");
+        File redu = new File("E:\\BaiduNetdiskDownload\\龙珠高清壁纸\\001redu.jpg");
+        // 加密处理
+        encyFile(origin, ency);
+        // 解密处理
+        reduFile(ency, redu);
     }
 
     /**
@@ -74,5 +88,70 @@ class IoApplicationTests {
         }
     }
 
+    void encyFile(File origin, File target) {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            fis = new FileInputStream(origin);
+            fos = new FileOutputStream(target);
+            // 加密处理
+            int b;
+            while((b = fis.read()) != -1) {
+                fos.write(b ^ 2);
+            }
+        }catch (Exception e) {
+
+        }finally {
+            if(fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    log.error("加密文件过程中, 出现输出流关闭异常");
+                    throw new RuntimeException(e);
+                }
+            }
+            if(fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    log.error("加密文件过程中, 出现输入流关闭异常");
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    void reduFile(File origin, File target) {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            fis = new FileInputStream(origin);
+            fos = new FileOutputStream(target);
+            // 加密处理
+            int b;
+            while((b = fis.read()) != -1) {
+                fos.write(b ^ 2);
+            }
+        }catch (Exception e) {
+
+        }finally {
+            if(fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    log.error("加密文件过程中, 出现输出流关闭异常");
+                    throw new RuntimeException(e);
+                }
+            }
+            if(fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    log.error("加密文件过程中, 出现输入流关闭异常");
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 
 }
